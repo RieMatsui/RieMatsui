f47c0192eb5630716348e1676c51a52dd2035f38
松井 里衣 <funkybass0405@gmail.com>
2016-12-28 00:18:46 +0900

    ログインしたユーザーでつぶやいた内容を表示

--- 会員登録/index.php
+++ 会員登録/index.php
@@ -32,6 +32,12 @@ if(!empty($_POST)){
         exit();
     }
 }
+//投稿を取得する
+?>
+<?php
+ $sql = sprintf('SELECT m.name, m.picture, p.* FROM members m,
+    posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+ $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -59,6 +65,26 @@ if(!empty($_POST)){
          <input type="submit" value="投稿する"/>
      </div>
     </form>
+    <br>
+    <?php
+    while($post = mysqli_fetch_assoc($posts)):
+    ?>
+        <div class="msg">
+        <img src ="member_picture/<?php echo htmlspecialchars($post['picture'],ENT_QUOTES,'UTF-8');?>"
+        width="48" height="48" alt="<?php echo htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8');?>"/>
+
+        <p>
+            <?php echo htmlspecialchars($post['message'],ENT_QUOTES,'UTF-8');?>
+            <span class="name">(<?php echo  htmlspecialchars($post['name'],ENT_QUOTES,'UTF-8');?>)</span>
+        </p>
+        <p class="day">
+            <?php echo htmlspecialchars($post['created'],ENT_QUOTES,'UTF-8');?>
+        </p>
+        </div>
+        <?php
+        endwhile;
+        ?>
+
 </div>
 <div id="foot">
 <p><img src="images/txt_copyright.png" width="136" height="15" alt="(C) H2O Space. MYCOM" /></p>
