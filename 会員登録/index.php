fa3c5dad7b038e348f866cd23fc8d867633d45df
松井 里衣 <funkybass0405@gmail.com>
2016-12-27 23:06:37 +0900

    投稿画面を作成

--- 会員登録/index.php
+++ 会員登録/index.php
@@ -1,3 +1,38 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+    //ログインしている
+    $_SESSION['time'] = time();
+
+    $sql = sprintf('SELECT * FROM members WHERE id=%d',
+        mysqli_real_escape_string($db,$_SESSION['id']));
+        $record = mysqli_query($db, $sql) or die(mysqli_error($db));
+        $member = mysqli_fetch_assoc($record);
+    }
+
+    else{
+         //ログインしていない
+        header('Location:login.php');
+        exit();
+    }
+
+
+//投稿を記録する
+if(!empty($_POST)){
+    if($_POST['message'] !=''){
+        $sql = sprintf('INSERT INTO posts SET member_id=%d,message="%s",created=NOW()',
+        mysqli_real_escape_string($db,$member['id']),
+        mysqli_real_escape_string($db,$_POST['message'])
+        );
+        mysqli_query($db,$sql) or die(mysqli_error($db));
+
+        header('Location:index.php');
+        exit();
+    }
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -13,11 +48,18 @@
 </div>
 
 <div id="content">
-<p style="margin-top: 200px">
-
-</p>
+   <form action="" method="post">
+     <dl>
+        <dt><?php echo htmlspecialchars($member['name']); ?>さん、メッセージをどうぞ</dt>
+        <dd>
+        <textarea name = "message" cols="50" rows="5"></textarea>
+        </dd>
+     </dl>
+     <div>
+         <input type="submit" value="投稿する"/>
+     </div>
+    </form>
 </div>
-
 <div id="foot">
 <p><img src="images/txt_copyright.png" width="136" height="15" alt="(C) H2O Space. MYCOM" /></p>
 </div>
